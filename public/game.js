559140c675be1a41a433ffca1067cddaccc91ce0
Eoin McGrath <eoinmcg@gmail.com>
2024-08-12 18:07:08 +0300

    updates build

--- public/game.js
+++ public/game.js
@@ -953,7 +953,7 @@
             this.g.mainMusic.currentTime = 0;
           }
           this.g.H.toggleFullScreen(this.g.canvas.c);
-          this.g.changeState("Play");
+          this.g.changeState("Tutorial");
         }
       });
       this.g.addEvent({
@@ -971,9 +971,6 @@
       let g = this.g, i = g.input.keys;
       for (let e of this.g.ents)
         e.update(dt);
-      if (this.canStart && (i.x || i["Space"] || i["Enter"])) {
-        this.g.changeState("Play");
-      }
     }
     render() {
       const g = this.g;
@@ -1238,7 +1235,7 @@
       this.wiper.render();
     }
     killBaddie(o2) {
-      if (Math.random() > 0.5) {
+      if (Math.random() > 0.9) {
         this.g.spawn("Powerup", { p: this, x: o2.x, y: o2.y });
       }
       this.score += o2.val;
@@ -1259,7 +1256,6 @@
       level.bg.forEach((l) => {
         let ent = l.k;
         l.p = this;
-        console.log(ent, l);
         this.g.spawn(ent, l);
       });
       level.spawns.forEach((s) => {
@@ -1342,7 +1338,6 @@
       this.levelComplete = true;
       this.g.events = [];
       let cb = () => {
-        console.log("DONE", this.wiper);
         this.levelNum += 1;
         this.levelData = levels_default.levels[this.levelNum];
         if (!this.levelData) {
@@ -1532,6 +1527,96 @@
     }
   };
 
+  // src/game/states/tutorial.js
+  var Tutorial = class {
+    constructor(g) {
+      this.g = g;
+      this.mainText = g.H.mkFont(g, 1, 2);
+      this.mainTextShadow = g.H.mkFont(g, 1, 0);
+      this.secondText = g.H.mkFont(g, 1, 1);
+      this.canStart = false;
+      window.T = this;
+    }
+    init() {
+      const g = this.g;
+      this.step = 1;
+      g.spawn("Control", {
+        y: 53,
+        x: false,
+        w: 28,
+        clickCol: 11,
+        col: 10,
+        key: " ",
+        text: "READY",
+        cb: () => {
+          if (this.canStart) {
+            this.g.changeState("Play");
+          }
+        }
+      });
+      this.g.addEvent({
+        t: 100,
+        cb: () => {
+          this.canStart = true;
+        }
+      });
+      this.baddies = {
+        eye: new Sprite(this.g, { i: "eye", x: 30, y: 30 }),
+        fly: new Sprite(this.g, { i: "fly", frames: 3, x: 20, y: 30 }),
+        creep: new Sprite(this.g, { i: "creep", frames: 2, x: 40, y: 30 })
+      };
+      this.powerup = new Sprite(this.g, { i: "star", x: 30, y: 30, frames: 2, flip: { x: 1, y: 0 } });
+      this.updateStep();
+    }
+    update(dt) {
+      let g = this.g, i = g.input.keys;
+      for (let n of this.g.ents)
+        n.update(dt);
+      if (this.g.fader > 0) {
+        this.powerup.frame = 2;
+        this.baddies.creep.frame = 2;
+      } else {
+        this.powerup.frame = 1;
+        this.baddies.creep.frame = 1;
+      }
+    }
+    render() {
+      const g = this.g;
+      g.draw.clear(12);
+      for (let n of this.g.ents)
+        n.render();
+      if (this.step === 1) {
+        g.draw.text(`${g.mobile ? "TAP" : "CLICK"} TO SHOOT`, this.mainText, false, 10);
+        g.draw.text(`AND FLY`, this.mainText, false, 20);
+        g.draw.img(g.imgs["bee"], 28, 30, 2);
+      } else if (this.step === 2) {
+        g.draw.text(`SHOOT THE`, this.mainText, false, 10);
+        g.draw.text(`BADDIES`, this.mainText, false, 20);
+        this.baddies.fly.render();
+        this.baddies.eye.render();
+        g.draw.rect(31, 41, 1, 1, 3);
+        this.baddies.creep.render();
+      } else if (this.step === 3) {
+        g.draw.text(`GRAB THE`, this.mainText, false, 10);
+        g.draw.text(`POWERUPS`, this.mainText, false, 20);
+        this.powerup.render();
+      }
+      g.draw.img(g.imgs.target, g.input.m.x - 1, g.input.m.y - 1);
+    }
+    updateStep() {
+      this.g.addEvent({
+        t: 200,
+        cb: () => {
+          this.step += 1;
+          if (this.step > 3) {
+            this.step = 1;
+          }
+          this.updateStep();
+        }
+      });
+    }
+  };
+
   // src/game/entities/text.js
   var Text = class {
     constructor(g, o2) {
@@ -1671,7 +1756,6 @@
       this.origY = o2.y;
       if (!this.center) {
         this.tX = this.x + 4;
-        console.log("CENTER");
       }
     }
     update() {
@@ -2509,7 +2593,8 @@
     Title,
     Help,
     Play,
-    Win
+    Win,
+    Tutorial
   };
   o.ents = {
     Sprite,
